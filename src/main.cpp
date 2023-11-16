ba34001c0fcb149c7b071b2b17ce8cc68ce9267d
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-11-16 23:53:26 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,7 @@
  * Name:
  */
 
-#include <iostream>
 #include "task_1.h"
-
-int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
-    return 0;
-}
+// #include "task_2.h"
+// #include "task_3.h"
+// #include "task_4.h"
