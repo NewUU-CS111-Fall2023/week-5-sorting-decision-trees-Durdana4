d659d862e557fc219f125793a2d3e633c3679c2a
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-11-16 23:40:14 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,38 @@
  * Date:
  * Name:
  */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void bubbleSort(vector<string>& titles) {
+    int n = titles.size();
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (titles[j] > titles[j + 1]) {
+                swap(titles[j], titles[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    vector<string> bookTitles = {"The Catcher in the Rye", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Great Gatsby"};
+    
+    cout << "Original list of book titles:" << endl;
+    for (const auto& title : bookTitles) {
+        cout << title << endl;
+    }
+    
+    bubbleSort(bookTitles);
+    
+    cout << "\nSorted list of book titles:" << endl;
+    for (const auto& title : bookTitles) {
+        cout << title << endl;
+    }
+    
+    return 0;
+}
